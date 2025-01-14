19ea019d037e2e996004b9b315e048a43b9d9590
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2025-01-14 12:43:20 +0200

    add eslint && prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,24 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,tsx,jsx}"],
+    rules: {
+      semi: "error",
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+      "no-console": "error",
+      "no-constant-condition": "error",
+      "no-unsafe-negation": "error",
+      eqeqeq: "error",
+      "prefer-const": "error",
+      "no-trailing-spaces": "error",
+    },
+  },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
 ];
-
-export default eslintConfig;
