813d28f347dd12f566591ffbcea7565d168b6865
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2025-01-14 16:14:32 +0200

    feat: add a new feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
             Get started by editing <code>src/app/page.tsx</code>.
           </li>
           <li>Test Save and see your changes instantly.</li>
+          <li>new feature</li>
         </ol>
 
         <div className={styles.ctas}>
